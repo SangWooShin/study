32e5bb3d204403c35d093718b0242c9261dcdaa1
SangWooShin <jay1024@naver.com>
2016-10-14 02:15:31 +0900

    Change animation, Make car move smooth

--- Resource/move_character_esc.py
+++ Resource/move_character_esc.py
@@ -2,27 +2,34 @@ from pico2d import *
 
 def handle_events():
     global running
+    global iX
+    global iXcount
     events = get_events()
     for event in events:
+
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif event.key == SDLK_LEFT:
+                ++iXcount
     pass
 
 open_canvas()
 MainCharacter = load_image('MainCar.png')
 AiCharacter = load_image('PoliceCar.png')
-
 running = True
+iXcount = 0
 y = 0
 frame = 0
+iX = 400 - iXcount*100
 while (y < 600 and running):
     clear_canvas()
-    MainCharacter.clip_draw(0, 0, 100, 100, 400, y)
+    MainCharacter.clip_draw(0, 0, 100, 100, iX, y)
     AiCharacter.clip_draw(frame*100, 0, 100, 100, 300, y)
     update_canvas()
-    frame = (frame + 1) % 8
+    frame = (frame + 1) % 2
     y += 5
     delay(0.1)
     handle_events()
